2aab3cedc3b11c7034c11053067a9a2a0cbd6ab4
adelkesc <256523@via.dk>
2020-10-03 02:20:33 +0200

    Made test scene, do not mess with it.

---
+++ projectTwo/MarkerLessARtest/Assets/Scripts/CubeScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //This is supposed to access the resources folder in living birds and instantiate the cardinal.
+            GameObject instance = Instantiate(Resources.Load("lb_cardinalHQ")) as GameObject;
+        }
+    }
+}
