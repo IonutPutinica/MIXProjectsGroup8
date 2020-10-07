6474c6122a5ceba09ed9ddd2462f62bdd85861e4
adelkesc <256523@via.dk>
2020-10-07 05:01:12 +0200

    Added and deleted scripts.  Added folder in Scene.

---
+++ projectTwo/MarkerLessARtest/Assets/Scripts/BirdDespawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdDespawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DespawnTimer()
+    {
+        //StartCoroutine(Despawn());
+    }   
+    
+    private IEnumerator Despawn()
+    {
+        yield return new WaitForSeconds(5f);
+        //Access GameObject instance from another script to destroy it?
+        //May have to move CheckCube method to have access to instances...
+    }
+}
