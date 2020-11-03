fb1bbe3aac516d715f6f9e65b500ee5cde0846e8
adelkesc <256523@via.dk>
2020-11-03 16:10:26 +0100

    Added sound effect to Audio Manager and added light switch.

--- projectThree/XRGame/Assets/Scripts/LargeImpactFX.cs
+++ projectThree/XRGame/Assets/Scripts/LargeImpactFX.cs
@@ -5,23 +5,12 @@ using UnityEngine;
 public class LargeImpactFX : MonoBehaviour
 {
     //The audio played here is tied to a particular game object
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject)
         {
-            //play audio
+            AudioManagerScript.instance.Play("LargeObjectHit");
         }
     }
 }
--- projectThree/XRGame/Assets/Scripts/SoundScript.cs
+++ projectThree/XRGame/Assets/Scripts/SoundScript.cs
@@ -13,6 +13,17 @@ public class SoundScript  //The MonoBehavior was deleted for a reason.  This is 
     public float volume;
     public bool loop;
 
+    //No idea if this will work for 3D.  Delete immidiately if it doesn't.
+    [Range(0f, 256f)]
+    public float priority;
+    [Range(0f, 1f)]
+    public float spatialBlend;
+    [Space]
+    public float minDistance;
+    [Space]
+    public float maxDistance = 500;
+    //Not anything after this though.
+
     [HideInInspector]
     public AudioSource source;
     // This class is used to define and expose AudioClip attributes in the Audio Manager.
