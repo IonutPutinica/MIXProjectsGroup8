dd690ef587ac6d4d7fe8a20d4c1c0b28899f16d8
HSD112 <horvathstefandaniel@gmail.com>
2020-11-03 19:18:29 +0100

    Hands are working

--- projectThree/XRGame/Assets/Hando/HandScript.cs
+++ projectThree/XRGame/Assets/Hando/HandScript.cs
@@ -7,34 +7,11 @@ public class HandScript : MonoBehaviour
 {
     public GameObject openHand;
     public GameObject closedHand;
-    public GameObject grabbedObjectPosition;
-    public GameObject camera;
+    public bool isLeftHand;
 
     SphereCollider openHandInteractionSphere;
 
-    public AudioSource pootisTest;
-
     bool handOpenBool = true;
-    bool holdingObject = false;
-    bool canGrab = false;
-    GameObject grabbedObject;
-
-
-    // Start is called before the first frame update
-    void checkForInteractiveObject()
-    {
-        RaycastHit hitInfo;
-        if (Physics.Linecast(openHand.transform.position, openHand.transform.position+camera.transform.forward*1000,out hitInfo, 9))
-        {
-            pootisTest.Play();
-            grabbedObject = hitInfo.collider.gameObject;
-            holdingObject = true;
-            print("Intersected obhect" + grabbedObject.name.ToString());
-            
-        }
-        Debug.DrawLine(openHand.transform.position, openHand.transform.position + openHand.transform.forward * 100f, Color.red, 10f);
-        Debug.DrawRay(openHand.transform.position, openHand.transform.forward);
-    }
 
     void Start()
     {
@@ -47,40 +24,33 @@ public class HandScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if( (isLeftHand == false && Input.GetButton("XRI_Right_GripButton"))    || 
+            (isLeftHand == true && Input.GetButton("XRI_Left_GripButton"))      ||  //gets input from left or right hand
+            Input.GetMouseButton(1)) //debug
         {
-            handOpenBool = !handOpenBool;
-            Debug.DrawLine(openHand.transform.position, openHand.transform.position + openHand.transform.forward * 100f, Color.red, 10f);
-            Debug.DrawRay(openHand.transform.position, openHand.transform.forward);
-            checkForInteractiveObject();
-
-            if (!handOpenBool)
-            {
-                closedHand.GetComponent<Renderer>().enabled = true;
-                closedHand.GetComponent<MeshCollider>().enabled = true;
-                openHand.GetComponent<Renderer>().enabled = false;
-            }
-            else
-            {
-                closedHand.GetComponent<Renderer>().enabled = false;
-                closedHand.GetComponent<MeshCollider>().enabled = false;
-                openHand.GetComponent<Renderer>().enabled = true;
-            }
-
-            print(Physics.Linecast(camera.transform.position, camera.transform.position + camera.transform.forward.normalized * 1000f, 9));
+            //handOpenBool = true;
+            closedHand.GetComponent<Renderer>().enabled = true;
+            closedHand.GetComponent<MeshCollider>().enabled = true;
+            openHand.GetComponent<Renderer>().enabled = false;
         }
-        
-
-        if (holdingObject && !handOpenBool)
+        else
         {
-            grabbedObject.transform.position = grabbedObjectPosition.transform.position;
+            closedHand.GetComponent<Renderer>().enabled = false;
+            closedHand.GetComponent<MeshCollider>().enabled = false;
+            openHand.GetComponent<Renderer>().enabled = true;
         }
-        else if (holdingObject && handOpenBool)
+        /*
+        if (!handOpenBool)
         {
-            holdingObject = false;
-            grabbedObject = null;
+            closedHand.GetComponent<Renderer>().enabled = true;
+            closedHand.GetComponent<MeshCollider>().enabled = true;
+            openHand.GetComponent<Renderer>().enabled = false;
         }
-
-
+        else
+        {
+            closedHand.GetComponent<Renderer>().enabled = false;
+            closedHand.GetComponent<MeshCollider>().enabled = false;
+            openHand.GetComponent<Renderer>().enabled = true;
+        }   */
     }
 }
