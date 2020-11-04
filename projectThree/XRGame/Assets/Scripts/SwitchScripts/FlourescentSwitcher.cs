632d84a3700a494686da6948e025841be41d8c2b
adelkesc <256523@via.dk>
2020-11-04 14:00:15 +0100

    Updated fluorescent light switch methods.

--- projectThree/XRGame/Assets/Scripts/SwitchScripts/FlourescentSwitcher.cs
+++ projectThree/XRGame/Assets/Scripts/SwitchScripts/FlourescentSwitcher.cs
@@ -7,15 +7,38 @@ public class FlourescentSwitcher : MonoBehaviour
     public Material materialSwap1;
     public Material materialSwap2;
 
-    [SerializeField]
-    private MeshRenderer original; //need to get it at element 1 in the light prefab
+    //[SerializeField]
+    //private MeshRenderer original; //need to get it at element 1 in the light prefab
 
+    GameObject[] fluorescentLights;
+    [SerializeField]
+    private MeshRenderer originalMeshes;
+
+    private void Start()
+    {
+        fluorescentLights = GameObject.FindGameObjectsWithTag("FluorLight");
+        Debug.Log("Object Number: " + fluorescentLights.Length);
+    }
     public void SetToOn()
     {
-        original.material = materialSwap1;
+        foreach (GameObject l in fluorescentLights)
+        {
+            originalMeshes = l.GetComponent<MeshRenderer>();
+            Material[] materials = originalMeshes.materials;
+            materials[1] = materialSwap1;
+        }
+        //original.material = materialSwap1;
+        //originalMeshes.material = materialSwap1;
     }
     public void SetToOff()
     {
-        original.material = materialSwap2;
+        foreach (GameObject l in fluorescentLights)
+        {
+            originalMeshes = l.GetComponent<MeshRenderer>();
+            Material[] materials = originalMeshes.materials;
+            materials[1] = materialSwap2;
+        }
+        //original.material = materialSwap2;
+        //originalMeshes.material = materialSwap2;
     }
 }
