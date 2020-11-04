5c939d578904fb1d77024aa2e09b7cf455406a08
adelkesc <256523@via.dk>
2020-11-04 11:54:07 +0100

    Added point and area light scripting.

---
+++ projectThree/XRGame/Assets/Scripts/SwitchScripts/FlourescentSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlourescentSwitcher : MonoBehaviour
+{
+    public Material materialSwap1;
+    public Material materialSwap2;
+
+    [SerializeField]
+    private MeshRenderer original; //need to get it at element 1 in the light prefab
+
+    public void SetToOn()
+    {
+        original.material = materialSwap1;
+    }
+    public void SetToOff()
+    {
+        original.material = materialSwap2;
+    }
+}
