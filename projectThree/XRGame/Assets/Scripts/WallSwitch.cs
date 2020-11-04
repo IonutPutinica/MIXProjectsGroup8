8ed910903458fa6a48030cc0528f38ccf6d03f69
adelkesc <256523@via.dk>
2020-11-04 10:17:12 +0100

    Creating lightmap and light switchers.

---
+++ projectThree/XRGame/Assets/Scripts/WallSwitch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSwitch : MonoBehaviour
+{
+    public Material materialSwap1;
+    public Material materialSwap2;
+
+    [SerializeField]
+    private MeshRenderer original;
+    private bool swapped = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (!swapped)
+            {
+                original.material = materialSwap1;
+                swapped = true;
+            }
+            else if (swapped)
+            {
+                original.material = materialSwap2;
+                swapped = false;
+            }
+        }
+    }
+}
