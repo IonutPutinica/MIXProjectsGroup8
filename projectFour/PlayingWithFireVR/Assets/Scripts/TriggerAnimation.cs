fe62f41131ebf4ef9765efe61746a275b058f4a8
Andrei Mungiu <andrei_mungiu@yahoo.com>
2020-11-11 02:40:46 +0300

    particle system and collision update
    added particle systems and made them look of adequate colour
    added script to particle systems which PLAY the particles on collision detection with the extinguisher and STOP the particles on exit

---
+++ projectFour/PlayingWithFireVR/Assets/Scripts/TriggerAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnimation : MonoBehaviour
+{
+
+    public Transform fireExtinguisher;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ParticleSystem emissionModule = fireExtinguisher.GetComponent<ParticleSystem>();
+        emissionModule.Stop();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ParticleSystem emissionModule = fireExtinguisher.GetComponent<ParticleSystem>();
+        emissionModule.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ParticleSystem emissionModule = fireExtinguisher.GetComponent<ParticleSystem>();
+        emissionModule.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
