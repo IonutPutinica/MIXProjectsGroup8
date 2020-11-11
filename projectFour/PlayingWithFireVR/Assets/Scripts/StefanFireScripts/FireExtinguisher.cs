1180a6e974e09bf8dd4cace3dd1596a080fd9d7e
HSD112 <horvathstefandaniel@gmail.com>
2020-11-12 00:53:44 +0100

    Added the fire extinguisher to the game scene, and added the pan with the fiery oil too. The scale is a bit different so I had to fix that.
    If you want to add particle effects to the pan, just add them as children and the script automatically stops them when they should be stopped.

---
+++ projectFour/PlayingWithFireVR/Assets/Scripts/StefanFireScripts/FireExtinguisher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireExtinguisher : MonoBehaviour
+{
+    public bool spewing;
+
+    public void toggle()
+    {
+        spewing = !spewing;
+
+        if (spewing)
+        {
+            enableTheExtinguisher();
+        }
+        else
+            disableTheExtinguisher();
+    }
+
+    private void disableTheExtinguisher()
+    {
+        GetComponentInChildren<ParticleSystem>().Stop();
+    }
+    private void enableTheExtinguisher()
+    {
+        GetComponentInChildren<ParticleSystem>().Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (spewing)
+        {
+            enableTheExtinguisher();
+        }
+        else
+            disableTheExtinguisher();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
