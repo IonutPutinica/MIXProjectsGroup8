8d36f59ea1caaed672b02e5cb3fa475d55453c87
adelkesc <256523@via.dk>
2020-11-14 04:37:40 +0100

    Audio Manager has been added with some background music.

---
+++ projectFour/PlayingWithFireVR/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound singleSound in sounds)
+        {
+            singleSound.source = gameObject.AddComponent<AudioSource>();
+            singleSound.source.clip = singleSound.clip;
+            singleSound.source.volume = singleSound.volume;
+            singleSound.source.loop = singleSound.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound by Name: " + name + "was not found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    private void Start()
+    {
+        Play("Background Music");
+    }
+}
