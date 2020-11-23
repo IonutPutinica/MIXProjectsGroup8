ff182569afb3457119639728b192420e38080c47
adelkesc <256523@via.dk>
2020-11-23 14:52:28 +0100

    Made minor change to FireScenario script.

--- projectFour/PlayingWithFireVR/Assets/Scripts/FireScenario.cs
+++ projectFour/PlayingWithFireVR/Assets/Scripts/FireScenario.cs
@@ -5,19 +5,18 @@ using UnityEngine;
 public class FireScenario : MonoBehaviour
 {
     public ParticleSystem smokeParticle;
-    public ParticleSystem fireParticle;
+    //public ParticleSystem fireParticle;
     public GameObject fireSource;
 
     private ParticleSystem smokeObject;
-    private ParticleSystem fireObject;
+    //private ParticleSystem fireObject;
 
     public IEnumerator StartFire()  //can only start the fire if it is stopped
     {
-        Debug.Log("FireStarted.");
         smokeObject = Instantiate(smokeParticle, fireSource.transform.position, Quaternion.identity);
         yield return new WaitForSeconds(3f);
         //fireObject = Instantiate(fireParticle, fireSource.transform.position, Quaternion.identity);
-        //might need to parent these gameobjects to the pan.
+
         FindObjectOfType<FireScript>().StartPanFire();
 
         smokeObject.Stop();
@@ -25,7 +24,7 @@ public class FireScenario : MonoBehaviour
     }
     public IEnumerator StopFire() // can only stop if the fire is already started
     {
-        fireObject.Stop();
+        //fireObject.Stop();
         //Destroy(fireObject);
         FindObjectOfType<FireScript>().StopPanFire();
         yield return null;
